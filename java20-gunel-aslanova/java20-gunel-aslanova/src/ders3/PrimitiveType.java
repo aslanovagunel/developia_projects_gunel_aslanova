f4a61d398c22ef6c4981725fd1fd0a230fc01c5c
Gunel Aslanova <aslanovagunel409@gmail.com>
2024-12-12 15:23:11 +0000

    primitivetype_task

---
+++ java20-gunel-aslanova/java20-gunel-aslanova/src/ders3/PrimitiveType.java
@@ -0,0 +1,16 @@
+package ders3;
+
+public class PrimitiveType {
+    public static void main(String[] args) {
+        double radius=5.2;
+        double lenght=2*3.14*radius;
+        System.out.println(lenght);
+
+        int number=67;
+        if (number%2==0){
+            System.out.println("cut");
+        }
+        else
+            System.out.println("tek");
+    }
+}
