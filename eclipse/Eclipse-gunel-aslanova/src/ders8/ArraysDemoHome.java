2582b3c19faaad5e87f208fbcc733e9945737ba0
Gunel Aslanova <aslanovagunel409@gmail.com>
2024-12-29 14:12:33 -0100

    ders8 Massivler_sortlama

--- eclipse/Eclipse-gunel-aslanova/src/ders8/ArraysDemoHome.java
+++ eclipse/Eclipse-gunel-aslanova/src/ders8/ArraysDemoHome.java
@@ -70,6 +70,25 @@ public class ArraysDemoHome {
 			System.out.print(numbers[i] + " ");
 		}
 
+		// sortlama
+//		13, 7, 5, 8, 12
+		System.out.println();
+		for (int i = 0; i < numbers.length; i++) {
+			for (int j = 0; j < numbers.length-1-i; j++) {
+				if (numbers[j] > numbers[j + 1]) {
+					int eded=numbers[j+1];
+					numbers[j + 1] = numbers[j];
+					numbers[j] = eded;
+					
+				}
+			}
+		}
+		for (int i = numbers.length - 1; i >= 0; i--) {
+			System.out.print(numbers[i] + " ");
+		}
+		
+
+
 
 	}
 
