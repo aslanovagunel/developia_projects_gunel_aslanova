41c8e0a61d3115c40a0a1a02ce4ab81d7d8186d3
Gunel Aslanova <aslanovagunel409@gmail.com>
2025-01-09 19:41:29 -0100

    massiv Variable Arguments

---
+++ eclipse/Eclipse-gunel-aslanova/src/ders11/SinifTap.java
@@ -0,0 +1,20 @@
+package ders11;
+
+public class SinifTap {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		SinifTap.method(1, 2, 3, 4, 5);
+
+	}
+
+	static void method(int... is) {
+		int sum = 0;
+		for (int a : is) {
+			sum += is[a - 1];
+		}
+		System.out.println(sum);
+	}
+
+}
